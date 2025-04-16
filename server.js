6063df742eef895a8776556b27f1402decd88598
AncientYoshi <paiswanpyae2002@gmail.com>
2025-04-16 19:59:48 +0630

    add public static to work sendfile

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const path = require("path");
 
-app = express();
+const app = express();
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/about/", (req, res) => {
-  res.send("About");
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
