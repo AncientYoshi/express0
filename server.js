0937fddb5fe41da1db417e01eb309b54ce302eef
AncientYoshi <paiswanpyae2002@gmail.com>
2025-04-16 20:11:51 +0630

    add json

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+//set up static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/about/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
+let posts = [
+  { id: 1, title: "Post 1", content: "This is the content of post 1" },
+  { id: 2, title: "Post 2", content: "This is the content of post 2" },
+  { id: 3, title: "Post 3", content: "This is the content of post 3" },
+  { id: 4, title: "Post 4", content: "This is the content of post 4" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
