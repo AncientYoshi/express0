b370f2eca5b9af9a23b02f6d2ee9709abe79db01
AncientYoshi <paiswanpyae2002@gmail.com>
2025-04-16 20:19:10 +0630

    added envfile to work

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 //set up static folder
