079a71244aa7209f1fd943a9cfbcb4ad861ce0bd
AncientYoshi <paiswanpyae2002@gmail.com>
2025-04-16 18:58:28 +0630

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+app = express();
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
